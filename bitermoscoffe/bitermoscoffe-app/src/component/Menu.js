387376caf2ff47801d5ac2cb939a17ddf5acdf5f
Engin Gülek <engingulek0@gmail.com>
2021-03-21 17:13:48 +0300

    MenuCreatePartI

---
+++ bitermoscoffe/bitermoscoffe-app/src/component/Menu.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Menu.css"
+function Menu() {
+  return (
+    <div className="menu">
+      <div className="menuList">
+        <ol className="list" >
+        <li>
+        Ismarla
+        </li>
+          <li>Kahve</li>
+          <li>Çay</li>
+
+    <li>Pasta</li>
+          <li>
+          Hakkımızda
+          
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
